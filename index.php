e84f0da541db25f5daeabd6f876132882a52ee97
Soham Shee <soham20s03h@gmail.com>
2023-03-12 11:01:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
                         if ($conn->connect_error) {
                         die("Connection failed: " . $conn->connect_error);
                         }
-                        $sql = "SELECT name, date, noi FROM events_info";
+                        $sql = "SELECT name, date, noi FROM events_info LIMIT 7";
                         $result = $conn->query($sql);
                         if ($result->num_rows > 0) {
                         // output data of each row
